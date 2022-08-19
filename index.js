f74c1b15ee105d8c40fb8fc96b84c7c5262dce10
asimcanuz <a.uz@quorion-qbs.com>
2022-08-19 17:43:45 +0300

    game type and game start button

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const game = {
-  xTurn: true,
-  xState: [],
-  oState: [],
+  xTurn: true, // true = X turn, false = O turn
+  xState: [], // array of X cells
+  oState: [], // array of O cells
+  gameType: 1, // 1 - player vs player, 2 - player vs computer
   winningStates: [
     ["0", "1", "2"],
     ["3", "4", "5"],
@@ -11,32 +12,87 @@ const game = {
     ["2", "5", "8"],
     ["0", "4", "8"],
     ["2", "4", "6"],
-  ],
+  ], // winning states
+  gameTableState: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 };
 
-document.addEventListener("click", (event) => {
-  const target = event.target;
-  const isCell = target.classList.contains("cell");
-  const isDisabled = target.classList.contains("disabled");
+function init() {
+  // initialize the game
+  game.xTurn = true; // X turn
+  game.xState = []; // empty array of X cells
+  game.oState = []; // empty array of O cells
+  game.gameTableState = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // empty game table state
+  game.winningStates = [
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"],
+    ["0", "3", "6"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["0", "4", "8"],
+    ["2", "4", "6"],
+  ]; // winning states
+}
 
-  if (isCell && !isDisabled) {
-    const cellValue = target.dataset.value;
-    game.xTurn === true ? game.xState.push(cellValue) : game.oState.push(cellValue);
-    target.classList.add("disabled");
-    target.classList.add(game.xTurn ? "x" : "o");
-    game.xTurn = !game.xTurn;
-  }
-  if (game.xState.length + game.oState.length === 9) {
-    handlerGameOver("Berabere!");
-  }
-  game.winningStates.forEach((state) => {
-    if (state.every((cell) => game.xState.includes(cell))) {
-      handlerGameOver("X kazandı!");
-    } else if (state.every((cell) => game.oState.includes(cell))) {
-      handlerGameOver("O kazandı!");
+const radioButtons = document.querySelectorAll('input[name="game-type"]'); // get the game type
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", (event) => {
+    // event listener for radio buttons
+    if (event.target.value === "player-vs-player") {
+      game.gameType = 1; // player vs player
+    } else {
+      game.gameType = 2; // player vs computer
     }
   });
 });
+
+if (game.gameType === 1) {
+  playerVplayer();
+} else {
+  playerVcomputer();
+}
+
+function playerVplayer() {
+  document.addEventListener("click", (event) => {
+    const target = event.target;
+    const isCell = target.classList.contains("cell");
+    const isDisabled = target.classList.contains("disabled");
+
+    if (isCell && !isDisabled) {
+      const cellValue = target.dataset.value;
+      game.xTurn === true ? game.xState.push(cellValue) : game.oState.push(cellValue);
+      target.classList.add("disabled");
+      target.classList.add(game.xTurn ? "x" : "o");
+      game.xTurn = !game.xTurn;
+    }
+    if (game.xState.length + game.oState.length === 9) {
+      handlerGameOver("Berabere!");
+    }
+    game.winningStates.forEach((state) => {
+      if (state.every((cell) => game.xState.includes(cell))) {
+        handlerGameOver("X kazandı!");
+      } else if (state.every((cell) => game.oState.includes(cell))) {
+        handlerGameOver("O kazandı!");
+      }
+    });
+  });
+}
+
+function playerVComputer() {
+  document.addEventListener("click", (event) => {
+    const target = event.target;
+    const isCell = target.classList.contains("cell");
+    const isDisabled = target.classList.contains("disabled");
+
+    if (isCell && !isDisabled) {
+      const cellValue = target.dataset.value;
+      game.xTurn === true ? game.xState.push(cellValue) : game.oState.push(cellValue);
+      target.classList.add("disabled");
+      target.classList.add(game.xTurn ? "x" : "o");
+      game.xTurn = !game.xTurn;
+    }
+  });
+}
 
 document.querySelector(".restart").addEventListener("click", () => {
   document.querySelector(".game-over").classList.remove("visible");
